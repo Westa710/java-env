83e50fcbe8bd1c4996def49559821902faace00a
Westa710 <pleiades1424@gmail.com>
2026-04-25 03:02:26 +0000

    chore: ビルド成果物をいったん追加

--- src/main/java/example1/example.java
+++ src/main/java/example1/example.java
@@ -4,5 +4,8 @@ public class example {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("changed");
+
+        int a = 100;
+        System.out.println(a);
     }
 }
